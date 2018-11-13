78bca6965cf9c140c27816278432c82947fd7cd3
otavis <otavio.cts@gmail.com>
2018-11-13 15:05:54 -0200

    Simplificada função buscaLoop

--- sudoku.js
+++ sudoku.js
@@ -6,15 +6,15 @@ let busca;
 let delay = 1; // velocidade da busca
 
 let matrizDesenho = [
-  [0, 0, 0, 6, 0, 0, 4, 0, 0],
-  [7, 0, 0, 0, 0, 3, 6, 0, 0],
-  [0, 0, 0, 0, 9, 1, 0, 8, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 5, 0, 1, 8, 0, 0, 0, 3],
-  [0, 0, 0, 3, 0, 6, 0, 4, 5],
-  [0, 4, 0, 2, 0, 0, 0, 6, 0],
-  [9, 0, 3, 0, 0, 0, 0, 0, 0],
-  [0, 2, 0, 0, 0, 0, 1, 0, 0]
+  [0, 2, 0, 6, 0, 8, 0, 0, 0],
+  [5, 8, 0, 0, 0, 9, 7, 0, 0],
+  [0, 0, 0, 0, 4, 0, 0, 0, 0],
+  [3, 7, 0, 0, 0, 0, 5, 0, 0],
+  [6, 0, 0, 0, 0, 0, 0, 0, 4],
+  [0, 0, 8, 0, 0, 0, 0, 1, 3],
+  [0, 0, 0, 0, 2, 0, 0, 0, 0],
+  [0, 0, 9, 8, 0, 0, 0, 3, 6],
+  [0, 0, 0, 3, 0, 6, 0, 9, 0]
 ];
 
 /*
@@ -109,20 +109,12 @@ function buscaLoop(){
     clearInterval(buscaTimer);
     return;
   }
-  buscaStep();
-  if(concluido){
-    clearInterval(buscaTimer);
-    return;
-  }
-}
-
-function buscaStep(){
   let atual = fronteira.pop();
   matrizDesenho = copiaMatriz(atual);
   explorados.push(atual);
-  concluido = (qtdElementos(atual) === 81) // meta
-  if(concluido){
-    return
+  if(qtdElementos(atual) === 81){ // meta
+    clearInterval(buscaTimer);
+    return;
   }
   if(busca=='cega'){
     let sucessores = getSucessoresCega(atual);
